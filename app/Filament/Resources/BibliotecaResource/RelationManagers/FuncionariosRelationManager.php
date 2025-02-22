ad702d55c8846a90c502f3e62bf2d18c48d159e1
rafaelsdomingos <rafaelsdomingos@gmail.com>
2025-02-22 19:55:35 -0300

    Atualiza o relation-manager dos funcionarios

--- app/Filament/Resources/BibliotecaResource/RelationManagers/FuncionariosRelationManager.php
+++ app/Filament/Resources/BibliotecaResource/RelationManagers/FuncionariosRelationManager.php
@@ -21,6 +21,36 @@ class FuncionariosRelationManager extends RelationManager
                 Forms\Components\TextInput::make('nome')
                     ->required()
                     ->maxLength(255),
+
+                Forms\Components\Select::make('funcao')
+                    ->required()
+                    ->options([
+                        'Coordenador(a)' => 'Coordenador(a)',
+                        'Biblitecário(a)' => 'Biblitecário(a)',
+                        'Secretário(a)' => 'Secretário(a)',
+                    ]),
+                
+                Forms\Components\Select::make('escolaridade')
+                    ->required()
+                    ->options([
+                        'Educação Básica' => 'Educação Básica',
+                        'Ensino Médio' => 'Ensino Médio',
+                        'Graduação' => 'Graduação',
+                        'Pós-Graduação' => 'Pós-Graduação',
+                    ]),
+            
+                Forms\Components\TextInput::make('fone')
+                    ->maxLength(255)
+                    ->default(null),
+
+                Forms\Components\TextInput::make('celular')
+                    ->maxLength(255)
+                    ->default(null),
+                
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->maxLength(255)
+                    ->default(null),
             ]);
     }
 
@@ -30,6 +60,9 @@ class FuncionariosRelationManager extends RelationManager
             ->recordTitleAttribute('nome')
             ->columns([
                 Tables\Columns\TextColumn::make('nome'),
+                Tables\Columns\TextColumn::make('funcao'),
+                Tables\Columns\TextColumn::make('escolaridade'),
+                Tables\Columns\TextColumn::make('celular'),
             ])
             ->filters([
                 //
